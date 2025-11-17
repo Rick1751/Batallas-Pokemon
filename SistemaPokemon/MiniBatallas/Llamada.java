e876fb1a49745db0c35069acb6f5c4c5818a9a41
Rick1751 <leonr7393@gmail.com>
2025-11-17 18:14:55 -0500

    Cambio el nombre a SistemaPokemon

---
+++ SistemaPokemon/MiniBatallas/Llamada.java
@@ -0,0 +1,19 @@
+package SistemaPokemon.MiniBatallas;
+
+public class Llamada {
+
+    public static void main(String[] args) {
+        Pokemon pikachu = new Pokemon("Pikachu", "Electrico", "Amarillo",
+                1, 20, 1, 1, 55.23, 2.00, true, 0.0, "Raichu",
+                5, 3, "ElectroBola",
+                "Cola de hierro", 2, 0);
+
+
+        // Llamada al metodo de inicializacion de parámetros
+        System.out.println("--------------------------------------------------------------------------------");
+        pikachu.sayInfo();
+
+
+
+    }
+}
