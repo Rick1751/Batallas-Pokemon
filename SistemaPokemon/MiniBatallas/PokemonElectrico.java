2776c338f64f63c6d771df1f552e641a49b67949
Rick1751 <leonr7393@gmail.com>
2025-11-17 23:49:59 -0500

    Version 1.0 Combates Pokemon: Plantillas de creacion

--- SistemaPokemon/MiniBatallas/PokemonElectrico.java
+++ SistemaPokemon/MiniBatallas/PokemonElectrico.java
@@ -2,27 +2,19 @@ package SistemaPokemon.MiniBatallas;
 
 public class PokemonElectrico extends Pokemon {
 
-    PokemonElectrico(int Id, int nivel, int vida,
-             int edad, int altura, double peso, int ataque,
-            int defensa, String ataqueEspecial,
-            String defensaEspecial, int velocidad, int experiencia) {
+    PokemonElectrico(String nombre, String color, int nivel, int vida,
+            int generacion, int edad, double altura, double peso,
+            boolean puedeEvolucionar, double nivelEvolucion, String nombreEvolucion,
+            int ataque, int defensa, String ataqueEspecial, String defensaEspecial,
+            int velocidad, int experiencia) {
 
-        super(1,"Pikachu", "Electrico", "Amarillo", nivel, vida,
-                1, edad, altura, peso, true, nivel,
-                "Raichu", ataque, defensa, ataqueEspecial,
-                defensaEspecial, velocidad, experiencia);
+        super(nombre, "Electrico", color, nivel, vida, edad, experiencia, altura, peso, puedeEvolucionar, nivelEvolucion,
+                nombreEvolucion, ataque, defensa, ataqueEspecial, defensaEspecial, velocidad, experiencia);
+
     }
+
+
 
     
-    PokemonElectrico(int Id_,int nivel, int vida,
-             int edad, int altura, double peso, int ataque,
-            int defensa, String ataqueEspecial,
-            String defensaEspecial, int velocidad, int experiencia) {
-
-        super(2,"Pikachu", "Electrico", "Amarillo", nivel, vida,
-                1, edad, altura, peso, true, nivel,
-                "Raichu", ataque, defensa, ataqueEspecial,
-                defensaEspecial, velocidad, experiencia);
-    }
 
 }
--- SistemaPokemon/MiniBatallas/main.java
+++ SistemaPokemon/MiniBatallas/main.java
@@ -5,13 +5,25 @@ public class main {
     public static void main(String[] args) {
 
         //Creacion de un pokemon electrico Pikachu
-        PokemonElectrico pikachu = new PokemonElectrico(0, 20, 1, 
-        70, 3.5, 0, 0, "Impac Trueno", "Esquivar", 
+        PokemonElectrico pikachu = new PokemonElectrico("Pikachu", "Amarillo",
+         0, 30, 1, 1, 70, 22, true,
+        0, "Raichu",
+        5, 3, "Impact Trueno", null, 
         2, 0);
+
+        //Creacion de un pokemon electrico magnemite
+        PokemonElectrico magnemite = new PokemonElectrico("Magnemite", "Gris", 
+        0, 25, 1, 1, 6.0, 22, true, 30.0, "Magneton",
+        10, 4, "Rayo", "Supersónico",
+        45, 0);
+    
 
         // Llamada al metodo de inicializacion de parámetros
         System.out.println("--------------------------------------------------------------------------------");
+
         pikachu.sayInfo();
+        magnemite.sayInfo();
+        
         
 
     }
