9df2d742da04afff072a0857950b69c61925f595
Rick1751 <leonr7393@gmail.com>
2025-11-17 23:22:24 -0500

    Revertirs

--- SistemaPokemon/MiniBatallas/PokemonElectrico.java
+++ SistemaPokemon/MiniBatallas/PokemonElectrico.java
@@ -2,12 +2,24 @@ package SistemaPokemon.MiniBatallas;
 
 public class PokemonElectrico extends Pokemon {
 
-    PokemonElectrico(int nivel, int vida,
+    PokemonElectrico(int Id, int nivel, int vida,
              int edad, int altura, double peso, int ataque,
             int defensa, String ataqueEspecial,
             String defensaEspecial, int velocidad, int experiencia) {
 
-        super("Pikachu", "Electrico", "Amarillo", nivel, vida,
+        super(1,"Pikachu", "Electrico", "Amarillo", nivel, vida,
+                1, edad, altura, peso, true, nivel,
+                "Raichu", ataque, defensa, ataqueEspecial,
+                defensaEspecial, velocidad, experiencia);
+    }
+
+    
+    PokemonElectrico(int Id_,int nivel, int vida,
+             int edad, int altura, double peso, int ataque,
+            int defensa, String ataqueEspecial,
+            String defensaEspecial, int velocidad, int experiencia) {
+
+        super(2,"Pikachu", "Electrico", "Amarillo", nivel, vida,
                 1, edad, altura, peso, true, nivel,
                 "Raichu", ataque, defensa, ataqueEspecial,
                 defensaEspecial, velocidad, experiencia);
